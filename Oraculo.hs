0bde943bb3071b5dea2b1ec0b8dbbe9c5a5c83da
Diaz M Andrea V <106710264+AndreaDiMartin@users.noreply.github.com>
2023-10-21 17:07:15 -0400

    Se añaden las funciones de acceso

--- Oraculo.hs
+++ Oraculo.hs
@@ -20,3 +20,28 @@ crearOraculo prediccion = Prediccion prediccion
 -- Ramificar
 ramificar:: [String] -> [Oraculo] -> String -> Oraculo
 ramificar opciones oraculos pregunta = Pregunta pregunta (M.fromList (zip opciones oraculos))
+
+--Obtiene la prediccion
+prediccion :: Oraculo -> String
+prediccion (Prediccion s ) = s 
+prediccion (Pregunta s a) = error "Debe ingresar un prediccion"
+
+--Obtiene la pregunta 
+pregunta :: Oraculo -> String
+pregunta (Pregunta s op) = s
+pregunta (Prediccion s) = error "Debe ingresar una pregunta"
+
+--Optiene las opciones asociadas a una pregunta
+opciones ::  Oraculo -> Opciones
+opciones (Pregunta s op) = op
+opciones (Prediccion s) = error "Debe ingresar una pregunta"
+
+--Funcion para hallar el oraculo de un maybe oraculo
+maybeOraculo :: Maybe Oraculo -> Oraculo
+maybeOraculo (Just a) = a
+maybeOraculo Nothing = error "No es una pregunta valida"
+
+--Optiene el oraculo respuesta asociado a una pregunta y una respuesta dada
+respuesta :: Oraculo -> String -> Oraculo
+respuesta (Pregunta s op) preg = maybeOraculo(M.lookup preg op)
+respuesta (Prediccion s) _ = error "Debe ingresar una pregunta"
